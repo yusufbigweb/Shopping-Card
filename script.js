0eabfcbece04fdd0400fbd0667641e5505753a78
Yusuf Shaikh <yusufshaikh00071@gmail.com>
2025-06-25 19:07:53 +0530

    update

--- script.js
+++ script.js
@@ -1,82 +1,99 @@
-const api = 'https://fakestoreapi.com/products';
+const api = "https://fakestoreapi.com/products";
 
-
-const basket = []
+const basket = JSON.parse(localStorage.getItem("data")) || [];
 
 async function fetchData() {
+  const fetchHere = await fetch(api);
+  const convertJson = await fetchHere.json();
+  const productRow = document.getElementById("productRow");
 
-    const fetchHere = await fetch(api);
-    const convertJson = await fetchHere.json();
-    const productRow = document.getElementById('productRow');
+  convertJson.map((val) => {
+    const incre = val.id;
+    let search = basket.find((x) => x.id === incre) || [];
 
-    convertJson.map((val)=>{
-
-        const paramenter = val.id
-        productRow.innerHTML += `
-        
+    productRow.innerHTML += `
           <div class="product-col">
                 <div class="center-image">           
-                     <img src="${val.image}" alt="product loading..." width="200px">
+                     <img src="${
+                       val.image
+                     }" alt="product loading..." width="200px">
                 </div>
                         <h3 class="product-title">${val.title}</h3>
-                        <p class="description">${val.description.length > 125 ? val.description.slice(0, 125) + "..." : val.description}</p>
+                        <p class="description">${
+                          val.description.length > 125
+                            ? val.description.slice(0, 125) + "..."
+                            : val.description
+                        }</p>
                         <div class="quantity">
                             <div class="price"> $${val.price}</div>
                             <div class="quan-count">
-                                <div onclick="decrement(${paramenter})" class="decreing"><i class="bi bi-dash-lg"></i></div>
-                                <div id=${paramenter} class="showing-update"><span>0</span></div>
-                                <div onclick="increment(${paramenter})" class="increing"><i class="bi bi-plus-lg"></i></div>
+                                <div onclick="decrement(${incre})" class="decreing"><i class="bi bi-dash-lg"></i></div>
+                                <div id=${incre} class="showing-update"><span>${search.item === undefined ? 0 : search.item}</span></div>
+                                <div onclick="increment(${incre})" class="increing"><i class="bi bi-plus-lg"></i></div>
                             </div>
                         </div>
-                    </div>
-        `
-    })
+                    </div>`;
 
-}
+                })};
 
 
-
-fetchData()
-
-const increment = (paramenter)=>{
-    console.log(basket)
-
-    const search = basket.find((x)=> x.id === paramenter)
-
-    if(search === undefined){
-
-        basket.push({
-        id: paramenter,
-        item: 1
-    })
-
-    }else{
-        search.item += 1;
-    }
-
-    
-}
-const decrement = (paramenter)=>{
-    console.log(paramenter)
-}
-const update = ()=>{}
+fetchData();
 
 
 
+const increment = (incre) => {
+  const search = basket.find((x) => x.id === incre);
+
+  if (search === undefined) {
+    basket.push({
+      id: incre,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
 
 
+  localStorage.setItem("data", JSON.stringify(basket));
+
+  update(incre);
+};
+
+const decrement = (incre) => {
+  const search = basket.find((x) => x.id === incre);
+
+  if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+  localStorage.setItem("data", JSON.stringify(basket));
+
+  console.log(basket)
 
 
+  update(incre);
+};
 
+const update = (id) => {
+  const search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+
+  calculation();
+};
+
+const calculation = () => {
+  const cardQuantity = document.getElementById("card-quantity");
+  cardQuantity.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
 
 
 
 
 // https://www.youtube.com/watch?v=cT_ZYrS3tKc&t=4264s
 
-
-
-
 // create basic html, css product page
 // fetch date using freeproduct-api in javascript
-// 
+//
